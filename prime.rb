67231f4cd4eecd21f64ff3404f7a3bf3dd9e4934
Christopher Buggelli <cjab86@gmail.com>
2017-03-05 03:53:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+  if num < 0
+    return false
+  end
+    2.upto(Math.sqrt(num).ceil) do |i|
+        break if num%i==0
+        return true if i==Math.sqrt(num).ceil
+    end
+    return false
+end

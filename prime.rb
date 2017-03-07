35556159a9237687507087fe3a7cc4b5968dfefe
Isaac Peifer <iwpeifer@gmail.com>
2017-03-07 05:34:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(int)
+  if int <= 1
+    return false
+  end
+  range = (2..int - 1).to_a
+  range.each do |number|
+    puts number
+    if int % number == 0
+      return false
+    end
+  end
+  return true
+end

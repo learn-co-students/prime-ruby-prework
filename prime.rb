6ed8071e5d1202620befd44ee7a675a96b01fc8d
Hersh Patel <hershe.91@gmail.com>
2017-03-11 23:20:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(number)
+  array = (2..(number-1)).to_a
+  array.each do |divider|
+    if number % divider == 0
+        return false
+      end
+    end
+  if number < 2
+    return false
+  end
+  true
+end
+

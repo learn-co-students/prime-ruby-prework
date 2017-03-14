d0549a2330e1f8716d6fdb16ddeb0f5c956c60a3
Jason Sears <jsears4585@gmail.com>
2017-03-14 17:39:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  end
+  range = (2..Math.sqrt(num)).to_a
+  range.each do |int|
+    return false if num % int == 0
+  end
+  true
+end

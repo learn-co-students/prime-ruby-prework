c1fe3c46cd60bc7b2a417d3e7eb8d30bce781f16
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-03-15 23:06:22 -0400

    Primes

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  test_array = (1..num).to_a
+  result_array = []
+  prime_array = [1, num]
+  test_array.each do |testor|
+    if num % testor == 0
+      result_array << testor
+    end
+  end
+  result_array == prime_array ? true : false
+end

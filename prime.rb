58fb807862d573f1f1ef32d3db280aca3eff5ec6
James Ross <jross.nyc111@gmail.com>
2017-03-16 13:49:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+  range = (2...num)
+  if num <= 1
+    return false
+  end
+  range.each do |second_num|
+    if num % second_num == 0
+      return false
+    end
+  end
+  return true
+end

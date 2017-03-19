e2b25c15d3a3161034493d61820a0ac64f1d809a
Isaac Anderson <isaac.s.anderson@gmail.com>
2017-03-19 16:05:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(number)
+  return false if number < 2
+  for i in 2...number do
+    # should use memoization here to cut down on time
+    return false if number % i == 0
+  end
+  true
+end

23b6715e2106020b62adfb641172d3e662271d9e
github username <github email address>
2017-03-20 11:37:57 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(int)
+    factors = []
+    array = (2..int-1).to_a
+    array.each do |x|
+        modulo = int % x
+        factors.push modulo
+        end
+if factors.include?(0)==false && int >1
+    return true
+    else return false
+end
+end

1bc26792f2ac70217fad254a2cac5832fa574339
Alexandra Williams <alexandrawilliams.aiw@gmail.com>
2017-03-25 22:18:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+def prime?(num)
+
+  if num == 1 || num <= 0
+    return false
+  end
+
+  x = num-1
+  divis = (2..x).to_a
+  newray = []
+
+  divis.each do |elem|
+      newray << num%elem
+  end
+
+ if newray.include?(0) == true
+   return false
+ else
+   return true
+ end
+
+
+end

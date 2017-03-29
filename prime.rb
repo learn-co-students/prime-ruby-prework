0a04e089197c54c8982b478374433bd27671445b
Samuel Goldsmith <samuelzgoldsmith@gmail.com>
2017-03-29 17:13:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(number)
+  if number == 0 || number == 1 || number < 0 
+    false
+  else
+    for i in 2...number
+      return false if (number % i == 0)
+    end
+    true
+  end
+end

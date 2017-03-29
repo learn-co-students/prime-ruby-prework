0e40585a3d0b5ff5381d66aacaf1dab276c7739d
Gaby Stern <stern.gabrielle@gmail.com>
2017-03-29 21:23:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  else
+    range = (2..Math.sqrt(number)).to_a
+    if (range).any? { |x| number % x == 0 }
+      return false
+    else
+      return true
+    end
+  end
+end

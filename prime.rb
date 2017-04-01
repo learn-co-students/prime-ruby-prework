7ce9557981f65ca00bcaab43b6a814e7621e62bc
Mike Luo <zluo@alumni.berklee.edu>
2017-04-01 18:41:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(n)
+  val = true
+  i = 2
+  if n < i
+    val = false
+  end
+  while i < n do
+    if n % i == 0
+      val = false
+    end
+    i += 1
+  end
+  return val
+end

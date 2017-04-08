aa4ee5cd4bdc712b40e83f31257d73fda69cb6ce
yungpanko <jrjohns@mit.edu>
2017-04-08 17:13:06 -0400

    primed it

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(n)
+  if n < 2
+    false
+  else
+    m = 2..Math.sqrt(n)
+    m_array = m.to_a
+    test_array =[]
+
+    m_array.each do |m|
+      test_array << n % m
+    end
+    !test_array.include?(0)
+  end
+end
+
+    #m being every number > 1 that is less than the sqrt of n

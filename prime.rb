c41d741c32604cc315b02beb8f66765e219bc4e6
Annee Barrett <ahuang852@gmail.com>
2017-04-11 22:58:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(int)
+  prime = true
+  prime = false if int <= 1
+  count = 0
+  (1..int).each do |div|
+    if int % div == 0
+      count += 1
+    end
+  end
+  prime = false if count > 2
+  prime
+end

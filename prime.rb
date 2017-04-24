b59a34ca3c7f33650bbf6b2aa92f48f01d8152a9
Tyler Knight <tknightx31@gmail.com>
2017-04-24 00:16:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(int)
+
+return false if int < 2
+
+max_divide = (int/2).round
+
+  (2..max_divide).each do |num|
+    return false if int % num == 0
+  end
+
+  true
+
+
+end

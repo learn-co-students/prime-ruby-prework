c14eed26a41e5a482a71e90dd56ef6dd480a3b36
Charles Wooley <cwooley89@gmail.com>
2017-04-28 01:53:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,31 @@
 # Add  code here!
+def prime?(int)
+  if int > 1
+    range = Array(1..int - 1)
+    range.each do |num|
+      if num != 1
+        if int % num == 0
+          return false
+        end
+      end
+    end
+  else
+    return false
+  end
+  true
+end
+
+
+def zero_to_a_hundred
+  zero_to_a_hundred = Array(0..100) #real quick
+
+  zero_to_a_hundred.each do |num|
+    if prime?(num)
+      puts "#{num} is a prime number"
+    else
+      #puts "#{num} is not a prime number"
+    end
+  end
+end
+
+zero_to_a_hundred

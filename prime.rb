3d6d142410ce7309526575a95a6bf6e4d1a8c729
Kesean Woodhouse <kesean.woodhouse@gmail.com>
2017-05-21 04:26:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(value)
+
+  return false if value <= 1
+
+  numbers = (2..value-1).to_a
+  cases = []
+  numbers.each do |x|
+    cases << x
+    break if x*x > value
+  end
+  return false if cases.any? { |k| value % k == 0 }
+    return true
+end

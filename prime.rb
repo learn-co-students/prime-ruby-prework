b3337dae16e5bf57823f1cca03c509d323659681
gabidavila <gabidavila@gmail.com>
2017-06-15 14:35:08 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,24 +1,25 @@
 KNOWN_PRIME_NUMBERS = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
 def prime?(number)
-  if number <= 1 || number % 2 == 0
-    return false
-  end
 
   if KNOWN_PRIME_NUMBERS.include?(number)
     return true
   end
 
+  if number <= 1 || number % 2 == 0
+    return false
+  end
+
   sqrt = Math.sqrt(number)
 
-  if sqrt.class == Float
-    last = 3
+  last = 3
 
-    while (last <= sqrt.ceill) do
-      if(number % last == 0)
-        return false
-      end
-      last += 2
+  while (last <= sqrt.ceil) do
+    if (number % last == 0)
+      return false
     end
+    last += 2
   end
+
   true
 end

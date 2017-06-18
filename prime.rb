8491940faa25bae4ff309128b9c1a49358a5a1cd
David Squires <david.squires1@gmail.com>
2017-06-18 17:36:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
-# Add  code here!
+
+#def prime?(number)
+#  return false if number < 2
+#  numbers = (2...number).to_a
+#  numbers.each do |n|
+#    return false if number % n == 0
+#    i = 0
+#    numbers = numbers.each do |x|
+#      numbers = numbers.slice(0...i) if x > (number / n)
+#      numbers.shift if x % n == 0
+#      i += 1
+#    end
+#  end
+#  return true
+#end
+
+def prime?(number)
+	return false if number < 2
+	numbers = (2...number).to_a
+	numbers.each do |n|
+		if number % n == 0
+			return false
+		else
+			numbers = (n...number/n)
+		end
+	end
+	return true
+end

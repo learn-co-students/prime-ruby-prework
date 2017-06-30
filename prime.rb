e2b6662369095181ebe21d315581b26912a31803
Justin Manalad <jstn.mnld@gmail.com>
2017-06-30 16:38:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(n)
+
+  if n == 2
+    return true
+  elsif n < 2 || n % 2 == 0
+    return false
+
+  elsif
+    testArray = (2..(n-1)).to_a
+    testArray.each do |a|
+      if n % a == 0
+        return false
+      end
+    end
+    return true
+  end
+end

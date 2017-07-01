0cf7e6e738c50dfb5b1011055c21151179970380
joshlacey <williamjoshualacey@gmail.com>
2017-07-01 11:19:43 -0400

    prime number checker

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+
+def prime?(number)
+    prime_array = []
+    for n in 2..number-1
+       if number % n == 0
+        prime_array << n
+       end
+    end
+    if number == -1 || number == 0 || number == 1
+        return false
+    else
+        if prime_array == []
+            puts prime_array
+            return true
+            elsif prime_array != []
+            puts prime_array
+            return false
+        end
+    end
+end

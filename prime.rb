dc9e8b34a62599714068687f62a7ecc26363dd3a
Logan Emerson <logemerson@gmail.com>
2017-07-04 17:23:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+require 'pry'
+
+def prime?(num)
+  divisor = 2
+  prime = true
+  if num.abs == 1 || num.abs == 0
+    prime = false
+    #binding.pry
+  else
+    while divisor <= num.abs/2 && prime == true
+      if num.abs % divisor == 0
+        prime = false
+      end
+      divisor += 1
+    end
+    prime
+  end
+end

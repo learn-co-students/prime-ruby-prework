60f64ff9f34a2c836fe9de92434a6ee8ed0eb858
Don Leistman <donleistman@gmail.com>
2017-07-10 21:49:13 -0400

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+    array = (2...num).to_a
+    if num <= 1
+        return false
+    end
+    for element in array do
+        if num % element == 0
+            return false
+        end
+    end
+    return true
+end

ed3d77544f262767deb0b00b6650055d6bee4ac1
Todd Greenberg <tsgreenberg1217@gmail.com>
2017-08-08 01:01:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(num)
+list = (2...num)
+if num<2
+  return false
+end
+list.each do |item|
+  if num%item==0
+    return false
+  end
+end
+return true
+end
+
+puts prime?(3)
+puts prime?(4)

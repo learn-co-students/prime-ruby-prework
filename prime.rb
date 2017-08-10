27edc7bb31ac9b0113e8053130979c367d92fc9f
Ashe Austaire <AsheAustaire@gmail.com>
2017-08-10 16:42:17 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(n)
+    if n > 1
+        for d in 2..(n - 1)
+            if (n % d) == 0
+            return false
+            end
+        end
+        return true
+    else
+        false
+    end
+end

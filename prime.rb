3a84f679e2f6f58bc5be9f5a437fab13cfd87556
Ashe Austaire <AsheAustaire@gmail.com>
2017-08-24 11:41:22 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,12 +1,17 @@
-def prime?(n)
-    if n > 1
-        for d in 2..(n - 1)
-            if (n % d) == 0
-            return false
-            end
-        end
-        return true
-    else
-        false
+def prime?(num)
+
+    return false if num <= 1
+
+    #----
+
+    x = nil
+
+    2.upto(num - 1) do |counter|
+
+
+        num % counter == 0 ? x = false : x = true
+        break if x == false
+
     end
+    return x
 end

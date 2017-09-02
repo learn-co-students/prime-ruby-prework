b94ec0e1a2600bd4d11a27bd82eb2c9bef3c1e9e
Dick Ward <dick@thedickward.com>
2017-09-02 00:38:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(number)
+  if number < 2
+    false
+  elsif number == 2 || number == 3
+    return true
+  else
+    i = 2
+    until i == number
+      if number % i == 0
+        return false
+      end
+      i += 1
+    end
+    true
+  end
+end

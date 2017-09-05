6144809ba0ec76073ac9a23b39e0cc186668ac69
github username <github email address>
2017-09-05 14:24:07 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+
+def prime?(int)
+	if int < 2
+		return false
+	elsif int < 4
+		return true
+	elsif int % 2 == 0 || int % 3 == 0
+		return false
+	end
+
+	i = 5
+	while i*i <= int do
+		if int % i == 0 || int % (i+2) == 0
+			return false
+		end
+		i += 6
+	end
+	return true
+end
+
+puts prime?(-1)

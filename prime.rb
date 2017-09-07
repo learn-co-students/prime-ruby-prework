e9e8c4ee0c17b0442420d60519dd29454feb0373
Nick Hall <nickhall122@gmail.com>
2017-09-07 14:50:00 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+
+
+def prime?(number)
+ending = number-1
+count = 2..ending
+if number<=1
+  return false
+elsif
+  for denoma in count
+      if (number % denoma) == 0
+        return false
+      end
+    end
+        return true
+end
+end

8a2718f00a1737ff8a1d88a67ecbcb847b0e816b
Lina Rudashevski <lina.rudashevski@gmail.com>
2017-09-10 23:14:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+
+def prime?(number)
+
+if number <= 1
+  return false
+end
+
+ i = 2
+
+ while i < number
+   if number % i == 0
+     return false
+   end
+   i += 1
+ end
+
+return true 
+end

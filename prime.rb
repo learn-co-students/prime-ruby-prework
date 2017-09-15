a526a6e9215c91170072f846a2e3fa7663a8d3ea
MatteBru <mbruni616@gmail.com>
2017-09-15 13:15:02 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+#require "pry"
+def prime? (num)
+  return false if num <= 1
+  divisor = (2..Math.sqrt(num).to_i).to_a
+  divisor.each do |div|
+    return false if num % div == 0
+  end
+  true
+end
+#binding.pry

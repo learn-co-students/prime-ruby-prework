35bb7610039c7f4ba44e1c02a27223009c132cd2
Brian Boisvert <b.m.boisvert@gmail.com>
2017-09-17 17:31:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(int)
+  new_count = 2
+
+  if int == 0 or int == 1 or int < 0
+    return false
+  end
+
+  while new_count < int
+    if int % new_count != 0
+      new_count += 1
+    else
+      return false
+    end
+    end
+    return true
+end

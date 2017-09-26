cb354824f145491717dbd15ced100ee000e14223
George Gore <georgelgore@gmail.com>
2017-09-26 19:36:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  count = (1..number).count { |el| number % el == 0 }
+
+  if count > 2 || number < 2
+    return false
+  else
+    return true
+  end
+
+
+end

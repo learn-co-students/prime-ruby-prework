b63af66fd3c16d280b20946bc046b4bad3f1bc89
Jake <jakemacnaughton@gmail.com>
2017-09-27 12:43:05 -0400

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+
+def prime?(int)
+  if int <= 1
+    return false
+  else
+    array = (2..(Math.sqrt(int).floor)).to_a
+    array.each {|i| return false if int % i == 0}
+    true
+  end
+ end

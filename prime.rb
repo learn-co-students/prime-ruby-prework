692b2168132e2ce5ac2637ea0c6385c05fbe06a3
Tashawn Williams <tcw331@gmail.com>
2017-09-27 14:46:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -2,12 +2,12 @@ def prime?(n)
   if n.abs < 2
     return false
   else
-    for num in 2...n
-      if (n % num == 0)
+    (2...n).to_a.each {
+      |num|
+      if n % num == 0
         return false
       end
-    end
+    }
   end
-  
   true
 end

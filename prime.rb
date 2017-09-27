1375a7e834a843009a1421fbf85c72feac47ead0
Tashawn Williams <tcw331@gmail.com>
2017-09-27 14:40:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(n)
+  if n.abs < 2
+    return false
+  else
+    for num in 2...n
+      if (n % num == 0)
+        return false
+      end
+    end
+  end
+  
+  true
+end

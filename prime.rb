9d365b3ef8099ed6c95e811a91f5240fd7683d0b
Ellisa Shim <ellishim@gmail.com>
2017-09-27 15:56:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(integer)
+  range = (2...integer).to_a
+  new_range = []
+  range.each do |primes|
+    new_range << integer % primes
+  end
+
+  if new_range.include?(0)
+    false
+  elsif integer < 2
+    false
+  else
+    true
+  end
+end

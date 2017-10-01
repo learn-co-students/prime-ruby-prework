5a95095f9f71859c76d4c57a353dddcd046dc775
github username <github email address>
2017-10-01 17:07:41 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
-# Add  code here!
+def prime?(int)
+  if int > 1
+    sqrtOfInt = Math.sqrt(int);
+    puts "Square of #{int} is #{sqrtOfInt}."
+
+    quotients = []
+    numbersLessThanSqrt = Array(2..sqrtOfInt)
+
+    numbersLessThanSqrt.each do |value|
+      quotients.push(Float(int) / Float(value))
+    end
+
+    quotients.each do |value|
+      if value % 1 == 0
+        return false
+      end
+    end
+
+    return true
+
+  else return false
+    
+  end
+
+end

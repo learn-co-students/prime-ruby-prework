6052d34d9347a496110d84ac158a34b8edcf243c
Kelly S <kellydsample@gmail.com>
2017-10-12 20:50:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+def prime? (num)
+
+  if num%2===0 || num < 0 || num == 1
+    return false
+  else
+    range = (2...num).to_a
+
+    range.each do |item|
+      if num%item == 0
+        return false
+      end
+    end
+
+    range.each do |item|
+      if (item+1)%item == 0
+        return false
+      else
+        return true
+      end
+    end
+  end
+end 

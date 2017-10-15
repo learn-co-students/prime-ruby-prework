fdf0766c8fb8854db54bd986e45f7470b95d0113
Tav Hafner <tav.hafner@gmail.com>
2017-10-15 15:32:56 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+
+def prime?(number)
+  return false if number <= 1
+  range = (2...number).to_a
+  range.each do |num|
+    return false if number % num == 0
+  end
+  true
+end

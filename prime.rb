279f344e5cb8709c1bcdae9a966eba60a88e484b
Joseph Moloughney <jmoloughney25@gmail.com>
2017-10-28 17:59:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 # Add  code here!
 def prime?(number)
    return false if number < 2
-   (2...number-1).to_a.any? {|i| number % i == 0} ? false : true
+   (2..number-1).to_a.any? {|i| number % i == 0} ? false : true
  end

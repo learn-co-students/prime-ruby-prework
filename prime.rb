d5c53ba5f6fa678eda20390b2f1db776596d67ea
eal6gf <elum1234@gmail.com>
2017-10-28 00:31:18 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(number)
+  if number < 0 or number == 0 or number == 1
+    return false
+  end
+
+  factors = 2..number/2
+  a = true
+  factors.each do |factor|
+    if number % factor == 0 and number != factor
+      a = false
+    end
+  end
+  a
+end

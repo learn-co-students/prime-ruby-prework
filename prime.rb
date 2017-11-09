dc3f557fc4320c3e43b3212dd88df39d9f98e60a
Marvin Clerge <clergemarvin@gmail.com>
2017-11-09 06:14:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,16 +1,14 @@
 def prime?(num)
+  sqrt_array = *(1..(Math.sqrt(num).to_i)) if num > 1
+  return false if num <= 1
 
-sqrt_array = *(1..(Math.sqrt(num).to_i)) if num > 1
-return false if num <= 1
+  prime_counter = 1
 
-prime_counter = 1
-
-sqrt_array.each do |x|
-  if num % x == 0
-    prime_counter += 1
+  sqrt_array.each do |x|
+    if num % x == 0
+      prime_counter += 1
+    end
   end
-end
 
-prime_counter <= 2 ?  (return true) :  (return false)
-
+  prime_counter <= 2 ?  (return true) :  (return false)
 end

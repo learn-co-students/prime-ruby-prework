433a4ac9fc42f92288d78f8ae888f96dfe7c4d26
Garry <clergeg@gmail.com>
2017-11-09 16:44:10 -0500

    Lab Commit

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(number)
+  arr_num = (2...number).to_a
+
+  if number <= 1
+    return false
+  else
+
+    arr_num.each do | num |
+      if number % num == 0
+        return false
+      end
+    end
+  end
+
+  true
+end

64decd15e8d18e83cad83833890adce47e41880f
Leslie Hsu <chuckleberries@hotmail.com>
2017-11-11 18:42:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,10 +1,24 @@
 # Add  code here!
+=begin
 def prime?(number)
   is_prime = true
   is_prime = false if number <= 1
   for divisor in (2...number)
     if (number%divisor) == 0
     is_prime = false
+    end
+  end
+  is_prime
+end
+=end
+
+def prime?(number)
+  is_prime = true
+  is_prime = false if number <= 1
+  for divisor in (2...number)
+    if (number%divisor) == 0
+    is_prime = false
+    break
     end
   end
   is_prime

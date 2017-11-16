ec713f6ba62a8e6d42f50e1de70443ecb7297c0d
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-16 01:17:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(n)
+  num = 2
+  if n < 2
+    return false
+  end
+  until num == n
+    if n % num == 0
+      return false
+    else
+      num+= 1
+    end
+  end
+  return true
+end

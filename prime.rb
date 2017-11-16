0f34a04b754f7de4c7085296febd101ad237d412
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-16 19:21:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(int)
+  if int<2
+    return false
+  else
+    sqrt_of_int=Math.sqrt(int).floor
+    count=2
+    while sqrt_of_int>=count
+      if int % count == 0
+      return false
+      else
+        count +=1
+      end
+    end
+  end
+  return true
+end
+
+prime?(82)

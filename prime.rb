6a717d35b182401a0c7e8cc9cc43877ce98cff38
josephdlawson21 <josephdlawson21@gmail.com>
2017-11-17 12:25:00 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+
+def prime?(num)
+  if num <= 1
+    return false
+  end
+    for counter in 2 .. num-1 do
+      if num % counter == 0
+        return false
+      end
+    end
+  true
+end

1f36a9838cc72431d528c4608872c321dd43feb2
Matt Jackson <matjack9@gmail.com>
2017-12-01 20:09:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+  if num.abs < 3
+    return false
+  end
+  test = (2...num.abs).to_a
+  test.each do |current|
+    if num % current == 0
+      return false
+    end
+  end
+  true
+end

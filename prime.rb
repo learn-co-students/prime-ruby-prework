007f495835602aaa8fad6cecce94ef3f8513d762
Christopher Diep <diep.christopher@gmail.com>
2017-12-02 19:05:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(integer)
+  if integer < 2
+    return false
+  end
+  int_above_sqrt = Math.sqrt(integer).ceil
+  count_up_array = (2..int_above_sqrt).to_a
+  count_up_array.each do |num|
+    if integer % num == 0
+      return false
+    end
+  end
+  true
+end

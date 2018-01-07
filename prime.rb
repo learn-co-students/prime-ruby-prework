eaec6d11f42bd72ee8f3b9e6160d4a9276524949
Brooke Yalof <brookeyalof2017@u.northwestern.edu>
2018-01-07 16:45:58 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(number)
+
+  while number < 2 
+    return false
+  end
+
+  it_num = 2 
+
+  while it_num < number
+    if number % it_num == true
+      return false
+    else 
+      return true
+    end
+  end
+    
+  
+end
+
+  

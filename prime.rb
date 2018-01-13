972bb604b4a3869af0db98af850f7e20e91e176f
Jenn Cacace <jenn.cacace@gmail.com>
2018-01-13 19:16:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime? (num)
+  if num <= 1 
+    return false 
+  end 
+  for divide_by in 2..(num-1)
+   if num % divide_by == 0
+     return false 
+   end
+ end
+ true 
+end

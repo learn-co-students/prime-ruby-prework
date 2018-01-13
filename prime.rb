d3cc733f2edb24044dfa42f99aa742c9bed4aadb
allinthemind66 <r.nilon92@gmail.com>
2018-01-13 12:59:11 -0500

    Prime Ruby

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(integer)
+  if integer <= 1
+    return false
+  elsif integer <= 3
+    return true
+  end
+  for n in (2..integer)
+    if integer % n == 0 && n != integer
+      return false
+    end
+  end
+  return true
+end

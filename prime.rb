49c2ac287f59a5f4f0f9431205253b6ce7e82f0e
Helen Liutongco <liutongcoh@gmail.com>
2018-01-25 15:12:22 -0600

    Modify prime.rb

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+  is_prime = true
+  if num <= 2
+    is_prime = false
+  else
+    array = (2..num/2).to_a
+    for element in array do
+      if num % element == 0
+        is_prime = false
+        break
+      end
+    end
+  end
+  is_prime
+end

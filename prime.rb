6287c1587a97ee7440e00d83148d9dac6bf2de86
github username <github email address>
2018-01-29 13:41:52 -0500

    prime? method

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(num)
+under_ten_primes = {2 => true, 3 => true, 5 => true, 7 => true}
+  if num <= 1
+    return false
+ elsif under_ten_primes[num]
+    return true
+  elsif num % 2 == 0
+    return false
+  end
+check = num/2
+current = 3
+while current < check
+  if num % current == 0
+    return false
+  end
+  current += 2
+end
+return true
+end

45f296783e56bb14bee5defa6ba4dfece842939f
deepgpatel <deepgpatel93@gmail.com>
2018-03-05 20:51:52 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+  is_prime = true
+  if num <= 1
+    false
+  else
+(2...num).each do |i|
+  if num % i == 0
+    is_prime = false
+  end
+  end
+is_prime
+end
+end

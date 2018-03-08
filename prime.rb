36dd475a456b86803eacb9bf7320ef3d96b62bb9
Adam Cohn <adamcohn88@gmail.com>
2018-03-08 16:02:52 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(number)
+	max = number
+	integers = (2..max)
+	divisible =[]
+	integers.each do |denomerator|
+		if number % denomerator == 0.0 && denomerator != number
+			divisible << number
+		end
+		divisible
+	end
+	if divisible.include?(number) || number == 1 || number == -1 || number == 0
+		false
+	else
+		true
+	end
+end

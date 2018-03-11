7b22f7b0c6d00236d30a18f30299345fb797feb4
Daniel Brazel <dbrazel4@gmail.com>
2018-03-11 21:32:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(int)
+  if int == 1
+    return false
+  elsif int > 0
+    range = (2...int).to_a
+    p range
+    range.each do |num|
+      if int % num == 0
+        return false
+      end
+    end
+    true
+  else
+    return false
+  end
+end

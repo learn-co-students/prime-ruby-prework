ae8605e79d244e0fc203b55dcfaa9375221359a8
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-13 02:41:46 +0000

    first commit

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+
+def prime?(num)
+  if num < 2
+    return false
+  end
+
+  i = 2
+
+  loop do
+    if (num % i == 0)
+      return false
+    end
+
+    i += 1
+    break if i == num
+  end
+
+  true
+end

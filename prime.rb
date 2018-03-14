9f84500bf9b36b8532f1a6330a28acfad86c68e3
Matthew Talbot <matthewtalbot@matthews-mbp.home>
2018-03-14 16:35:14 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+  n = 2
+  if num < 2
+    return false
+  elsif num > 2
+     while n < num
+      return false if num % n == 0
+      n += 1
+     end
+  end
+  true
+end

07480ccb666da897b73b901b99a9bd384a26917f
LaneMiller Miller <chris.lane.miller@gmail.com>
2018-03-19 20:51:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+  divisor = 2
+  
+  if num <= 1
+    return false
+  else
+    while divisor < num
+      if num % divisor == 0
+        return false
+      end
+      divisor += 1
+    end
+  end
+  return true
+end

b2d2da50e6ac6ed39cd2dd3a2c0433758a1554df
Ryan Hu <ryanhuuu@gmail.com>
2018-03-20 02:28:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,13 @@
 def prime?(num)
-  for (i=num;i>0;i--)
+  if num<=1
+    return false
+  end
+  i = num -1
+  while i>1 do
     if num%i==0
       return false
     end
+    i -= 1
   end
   
   return true

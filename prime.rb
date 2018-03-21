9a45a2b954f179708f6d879e05186f418b25d5b9
jessebillard <jessebillard@mac.com>
2018-03-21 13:33:22 -0400

    Primes

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(n)
+   if n <= 1
+     false
+   elsif n >= 2
+     range = (2..Math.sqrt(n))
+     range.each { |x|
+        return false if n % x == 0
+     }
+      true
+   end
+end

493a632b2f6a8cafab5b0fd87f8bfdd638b3003f
Mike C <mikecronin92@gmail.com>
2018-03-22 04:51:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1 
+    return false 
+  end
+  test_nums = 2...num
+  test_nums.each {|test|
+    if num % test == 0
+      return false
+    end
+  }
+  return true
+end

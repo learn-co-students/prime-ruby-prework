be38df6ffe7f094faf5029e3df139f71d1438ef6
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-03-31 20:21:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+  if(num < 2)
+    return false 
+  end
+  start = 2 
+  range = (start...num)  
+  range.each do |n|
+    if(num% n ==0)
+      return false
+    end
+  end
+  return true 
+end

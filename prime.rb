27b032e869c991abcc5592451824454e98abebd0
munirkizilcali <munir.kizilcali@gmail.com>
2018-04-02 20:39:49 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+    divident = number - 1
+    while divident > 1 && number.divmod(divident)[1] != 0
+        divident -= 1
+    end
+    if divident == 1
+        true
+    else
+        false
+    end
+end

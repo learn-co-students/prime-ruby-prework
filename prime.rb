5be6b8b9897860a449c65537480eab03a2e20738
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-07 05:46:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime? (int)
+  if int < 2
+    return false
+  else
+    divisors = (2...int).to_a
+      divisors.each do |i|
+      if int % i == 0
+        return false
+      end
+    end
+  end
+  return true
+end

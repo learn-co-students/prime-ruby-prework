2c6b07278f6a84dd542ae8a6286c3d503dc59d66
JohnZBurnett <tuninator@gmail.com>
2018-04-10 12:33:18 -0400

    Wrote prime function.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+
+def prime?(num)
+  if num < 2
+    return false
+  else
+    range = (2...num).to_a
+    range.each do |val|
+      if num % val == 0
+        return false
+      end
+    end
+  end
+
+  return true
+end # Add  code here!

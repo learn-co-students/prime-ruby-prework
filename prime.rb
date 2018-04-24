07c0e274b5b517571cc07f7b847b54682abef2de
github username <github email address>
2018-04-24 14:33:52 +0200

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+
+  return false if number <= 1
+
+  array = (2..number-1).to_a
+
+  array.each do |num|
+  return false if number % num == 0
+  end
+
+  return true
+  
+end

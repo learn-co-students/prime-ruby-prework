4880d727665d0a60e988d92f6d876af35a57d24b
Anthony Lam <al3101@nyu.edu>
2018-05-06 04:04:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1 || number % 2 == 0 
+    return false
+  end 
+  
+  limit = number / 2 
+  for num in (2..limit)
+    if number % num == 0
+      return false 
+    end 
+  end 
+  return true 
+end
+
+ 

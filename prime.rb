6df3fe6b9724aefb7df73d5fee715552a7d2f003
github username <github email address>
2018-05-08 12:05:36 -0400

    Implemented prime numbers method

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(num)
+  num_of_divisors = 0
+  (1..num).to_a.each do |x|
+    if num.to_i % x == 0
+      num_of_divisors += 1
+    end
+  end
+  num_of_divisors == 2 ? true : false
+end

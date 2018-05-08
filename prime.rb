1f31963ea134413810fce4fd95a510b70942f186
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-08 14:17:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+def prime?(int)
+  if int <= 2
+    return FALSE
+  else
+    counter = int - 1
+    until counter < 2
+      if int % counter == 0
+        return FALSE
+      end
+       counter = counter - 1
+    end
+  end
+  TRUE
+end

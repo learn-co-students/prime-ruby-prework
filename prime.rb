b0125a140832b10c763a95b7fd9d33ba2bee0696
Devin Pierce <devinmpierce@gmail.com>
2018-05-09 17:35:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(n)
+  if (n == 2)
+    return true
+  end
+  if (n < 2)
+    return false
+  end
+  range = (2...n)
+  for num in range do
+    if (n % num == 0)
+      return false
+    end
+  end
+  true
+end

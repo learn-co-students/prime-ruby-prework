408368d49fcdd3fcd06a4e963bee3590808a8fe2
critsmet <36425627+critsmet@users.noreply.github.com>
2018-05-09 00:17:29 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(integer)
+  numbersUpToInteger = (2...integer).to_a
+  newArray = []
+  if integer < 2
+    return false
+  elsif integer == 2
+    return true
+  else
+    numbersUpToInteger.each do |x|
+      newArray.push(integer % x)
+      break if x*x > integer
+    end
+    if newArray.include?(0)
+      return false
+    else
+      return true
+    end
+  end
+end

446363a1b5b1b0531672c7af44829afd04074030
Alexandra Hernandez <alexandrahernandez@Alexandras-MBP.home>
2018-05-14 14:58:22 -0400

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(num)	
+	new_array = (2..num-1).to_a	
+	return false if num <= 1
+	for i in new_array do
+		if num % i == 0
+			return false
+		end	
+	end
+	return true
+end

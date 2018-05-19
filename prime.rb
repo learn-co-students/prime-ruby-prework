13777f536f144766f82d4120b0936bd136b7caa8
Alan Golman <golman.alan@gmail.com>
2018-05-19 20:59:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(n)
+  if n < 2  
+    return false 
+  else 
+    array = (2..(n-1)).to_a 
+    array.each do |x|
+      if (n % x) == 0 
+        return false 
+      end 
+    end   
+  end 
+  return true 
+end   

354a195f714516f18b07ace953ce9b1e82305108
iwilliams83 <indirajamurath@gmail.com>
2018-05-21 15:36:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(num)
+  if num <= 0 || num == 1
+    return false
+  elsif num >= 2
+    possible_factors = []
+    remainders = []
+    i = Math.sqrt(num).floor
+    while i >= 2
+      possible_factors.push(i)
+      i-=1
+    end
+    possible_factors.each {|x| remainders.push(num % x)}
+    if remainders.include?(0) == true
+      return false
+    else
+      return true
+    end
+  end
+end

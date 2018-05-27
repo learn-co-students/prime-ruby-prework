936dd9654a63c9519f3df727783f5e1234b88041
Stephen F <stephen@farver.io>
2018-05-27 20:57:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(integer)
+  prime_flag = true 
+  
+  if integer.abs < 2
+    prime_flag = false
+  end
+  
+  (2...integer.abs).each do |num|
+    if integer % num == 0 
+      prime_flag = false 
+    elsif integer.abs == 2 
+      prime_flag = true
+    end
+  end
+  
+  prime_flag
+end

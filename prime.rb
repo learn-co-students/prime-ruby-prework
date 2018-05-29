6af48ec515a38dc838c1f49dde68673b874ad46c
Gabriel.Batista <gabedbatista@gmail.com>
2018-05-29 00:44:13 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
-# Add  code here!
+require'benchmark'
+
+Benchmark.bmbm(7) do |bm|
+  bm.report('From my mind')do
+    def prime?(prime)
+      flag = true
+      
+      if prime <= 1
+        flag = false
+      end
+
+      for i in 2..(prime/2) do
+        if prime % i == 0
+          return flag = false
+        end
+      end
+
+      flag
+    end
+  end
+end
+
+
+  

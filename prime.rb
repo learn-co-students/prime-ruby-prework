dbaabc3d4e082fddb6e8d1777a03b623ef00cc46
jaredianmills <jaredianmills@gmail.com>
2018-05-31 16:56:54 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(int)
+  factors = []
+  (1..int).to_a.each do |n|
+    if int % n == 0
+      factors << n
+    end
+    break if factors.size == 3
+  end
+  factors.size == 2
+end

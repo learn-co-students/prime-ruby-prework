fd23d00c8ff11fd350c5e0bd110c666ac9925698
John Raber <jrab224@gmail.com>
2018-06-01 00:27:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(i)
+  if i <= 1
+    false
+  elsif i == 2
+    true
+  else
+    start = 2
+    limit = Math.sqrt(i)
+    while start <= limit
+      if i % start == 0
+        return false
+      end
+      start += 1
+    end
+    true
+  end
+end

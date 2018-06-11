73bdb8d465155509f36a7863af3a8f5d4fb826e4
kevinptx <kevin.peery@gmail.com>
2018-06-11 08:57:00 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(integer)
+  range = (2..(integer-1)).to_a
+  result = true
+  if integer < 2
+    result = false
+  else
+    range.each do |x|
+      if integer % x == 0
+        result = false
+        break
+      end
+    end
+  end
+  return result
+end

1f3c864e3833bb6c928da3b3713d66b058ec448d
Your Name <you@example.com>
2018-06-20 11:25:41 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,4 +1,9 @@
 # Add  code here!
 def prime?(n)
+  #finding prime number
 Prime.prime?(n)
+=begin Prime.prime(arguement) is a bulit in ruby function
+go to Spec folder and require 'Prime'
+if stuck go on stack overflow and look up Ruby prime method
+=end
 end

de7bf0b4e9ba0819089001e1f7b12569903ef033
Bryan Teng <bteng1226@gmail.com>
2018-06-24 01:11:04 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(int)
-  return false if  int == 0 || int == 1 
+  return false if int == 0 || int == 1 
   for num in (2...int).to_a
   sum = int%num
   return false if sum == 0

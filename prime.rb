6e2263059b49fc8872b9429809d2e97aa2ac7975
Dina Hafez <dinahafez.8@gmail.com>
2018-06-26 23:20:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+
+def prime?(num)
+  range = (2..num-1).to_a
+  is_prime = true
+  range.each do |i|
+    if num % i == 0 
+      is_prime = false
+    end
+  end
+  if num < 2
+    is_prime = false
+  end
+  return is_prime
+end
+
+

da510bd6a09e1138b1e4d1be61e68ed148beedc3
Kelly <devkells00@gmail.com>
2018-07-04 22:25:58 -0500

    Passed, have questions

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(n)
+   (+/-2..n-1).each do |divider|
+     return false if n % divider == 0
+   end
+end
+
+
+
+def prime_define(int)
+   prime_num = []
+
+   (+/-2..int).each do |int|
+     prime_num << int if prime_define(int)
+   end
+
+   prime_num
+end

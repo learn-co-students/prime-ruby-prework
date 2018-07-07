6bab5f47889399fabdef3f1d93e92abbafa7cd6a
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-07 22:02:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+
+def prime?(num)
+  if num < 1 
+    return false 
+    elsif loop do (var i = 2; i < num; i++) 
+      return true 
+    end
+  end
+  
+  
+  
+  
+  ## function isPrime(value) {
+    for(var i = 2; i < value; i++) {
+        if(value % i === 0) {
+            return false;
+        }
+    }
+    return value > 1;
+}

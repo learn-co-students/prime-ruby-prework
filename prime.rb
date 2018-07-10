869fb16292de4250e70489e710876fd15c658ebb
Michael Coleman <michaelcoleman19@gmail.com>
2018-07-10 22:16:47 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  end
+  for divisor in 2..number-1
+    if number % divisor == 0
+      return false
+    end
+  end
+  return true
+end

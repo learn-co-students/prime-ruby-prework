b89eb9a97e75f0c8b6d59ddbda5eecb785dae8f4
Adam Johnson <adamwjo@gmail.com>
2018-07-23 15:24:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+
+
+def prime?(num)
+  if num < 2
+    return false
+  elsif num > 1 && num < 4
+    return true
+  elsif num % 2 == 0
+    return false
+  else
+    arr = (4..num/2).to_a
+    arr.each do |i|
+      if num % i == 0
+        return false
+      end
+    end
+  end
+    true
+end

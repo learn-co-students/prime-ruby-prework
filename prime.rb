d46ed7b13c7aabff707a08c936b8b6b59a0bb2ef
Jordan Farkas <jordfark@gmail.com>
2018-07-27 01:56:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+# # Add  code here!
+
+def prime?(integer)
+  if integer < 2 
+    return false 
+  end 
+  range = (2..integer-1)
+  range.each do |int| 
+  if integer % int == 0  
+    return false
+end
+end 
+    return true 
+end
+

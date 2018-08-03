ce11b831321dc68a314a1ff7e4ac99e3d07bef44
richardmaccaw <ricci6@gmail.com>
2018-08-03 11:23:03 +0100

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime? (number)
+  if number <= 2
+    return false
+  end
+  for item in 2..(number - 1)
+   if (number % item) == 0
+    return false
+   end
+  end
+  true
+end
+prime?(27)

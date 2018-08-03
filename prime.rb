a01295d156ebeaeab40ec516ebacf5258df9d739
Jin Ai Yap <jinai.jyap@gmail.com>
2018-08-03 01:32:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(n)
+  prime = true
+  test = 2
+  n <= 2 ? prime = false :
+  while test <= Math.sqrt(n)
+    if n % test == 0
+      prime = false
+    end
+    test += 1
+  end
+  prime
+end

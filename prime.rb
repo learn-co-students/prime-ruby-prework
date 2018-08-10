665e817555521e86e2f7a3c21e7b9cdd8b05d2f3
Chelsea Crowson <chelsea.crowson@me.com>
2018-08-10 12:09:18 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime? (integer)
     if integer <= 1
         false
     else 
-        (2..integer/2).none? do |x| integer % x == 0
+        (integer/2).none? do |x| integer % x == 0
         end
     end
 end

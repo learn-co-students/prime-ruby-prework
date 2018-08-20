5f4202dcefe604bd62c7308cba1be3e3cfb451e5
braj011 <bhagyaraj01@gmail.com>
2018-08-20 11:19:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+
+
+
+
+def prime?(num)
+  return false if num <= 1
+  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  true
+end
+
+#.downto - Iterates block, passing decreasing values from int down to and including limit.

a87247cea4a24fc605301739bc9d7de71accd946
Kim Pham <pham.kim.a@gmail.com>
2018-08-22 14:49:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+  
+  if num <= 1
+    return false
+  elsif num == 2 || num == 3
+    return true
+  else
+    number_array = (2..(num**0.5).to_int).to_a
+
+    number_array.each do |number|
+      return false if num % number == 0
+    end
+    
+    return true
+  end
+end

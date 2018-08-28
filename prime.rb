342858e16139207a7b00bd181986078f878b8b4d
Rob Parker <into.the.parker@gmail.com>
2018-08-28 12:25:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  end
+  primeCheck = []
+    for counter in 2..number do
+        if number % counter == 0
+      primeCheck.push(counter)
+    end
+  end
+  return checker(primeCheck)
+end
+
+
+def checker(primeCheck)
+  if primeCheck.size > 1
+    return false
+  else
+    return true
+  end
+end
+prime?(11)

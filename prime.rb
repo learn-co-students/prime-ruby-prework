9be7d6ed86dce012876bf06d4376865d73bdd4ad
yoshinator <yoanante@gmail.com>
2018-09-18 18:30:11 -0400

    Prime number verifier

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(integer)
+  switch = false
+  possible_factors = []
+  if integer > 2
+    possible_factors = (2...integer).to_a
+  elsif integer < 2
+    possible_factors = (integer+1..-2).to_a
+  else switch = true
+  end
+    possible_factors.each do |number|
+      if integer % number == 0
+        switch = false
+        break
+      else
+        switch = true
+      end
+    end
+  switch
+end

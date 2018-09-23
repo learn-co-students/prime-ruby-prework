6c085aae0f91b273a9cf0635fec5f3783a9b4d12
intuitive-programmer <steven@intuitiveprogrammer.com>
2018-09-23 19:09:53 +0100

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,29 @@
 # Add  code here!
+=begin
+def prime?(number)
+  if number < 2
+    return false
+  end
+
+  i = 2
+  sqrt_number = Math.sqrt(number)
+  while i <= sqrt_number
+    if number % i == 0
+      return false
+    end
+    i += 1
+  end
+  return true
+end
+=end
+
+def prime?(number)
+  return false if number < 2
+  sqrt_number = Math.sqrt(number)
+  (2..sqrt_number).each do |i|
+    if number % i == 0
+      return false
+    end
+  end
+  return true
+end

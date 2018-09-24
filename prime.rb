2f6976325d8255dc22dfe840ee7c887d954ee6a5
Joe Huntington <jhuntington@tutanota.com>
2018-09-24 14:50:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+  isPrime = true
+  for d in 2..(number-1)
+    if(number % d == 0)
+      isPrime = false
+    end
+  end
+  if(number.abs <= 1)
+    isPrime = false
+  end
+  isPrime
+end
+
+
+puts prime?(-1)

5c951899f93c918a854797a02d75196b24cb45af
Jacob Menashi <jbmenashi@gmail.com>
2018-09-27 15:04:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1 
+    return false
+  else
+    range_array = (2...number).to_a 
+    range_array.each do |element|
+      if number % element == 0 
+        return false
+      end
+    end 
+    return true
+  end
+end

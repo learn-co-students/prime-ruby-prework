02abfc996b3b98c871efeb26a72075b349c62336
github username <github email address>
2018-10-03 18:58:08 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(int)
+  return false if (int.even? && int > 2) || int <= 1
+
+  int_half = int / 2
+  x = 3
+
+  while x < int
+    return false if int % x == 0
+    x += 1
+  end
+  true
+end

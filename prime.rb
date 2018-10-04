9729385261cbfca7971ecdcb2cc8ef33edda95f3
Austin Puckett <puckett_austin@yahoo.com>
2018-10-04 01:37:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  if num <= 2
+    false
+  end
+  if num > 2
+    num_range = (2..num - 1).to_a
+    num_range.each do |x|
+      if num % x == 0
+        false
+      end
+    end
+  end
+  true
+end

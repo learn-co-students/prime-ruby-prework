ae31bf0fb042b9205d320074bd244e554e7cb6ec
github username <github email address>
2018-10-13 16:16:42 +0300

    prime?(number) method added

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  elsif number <= 3
+    return true
+  else (2..number/2).none? do |x|
+    number % x == 0
+  end
+  end
+end

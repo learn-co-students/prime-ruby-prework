be916ecb6b33d666b5865efb888173578c6d2f1d
Connor Callahan <connorwcallahan@gmail.com>
2018-10-15 22:29:13 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+ def prime?(int)
+  if int < 2 
+    return FALSE
+  end
+  (2...int).to_a.each do |num|
+    if int % num == 0
+    return FALSE
+  end
+end
+return TRUE
+end
+ 

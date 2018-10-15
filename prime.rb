3acb00570e94324f66a1a615df8de357c161f5d7
artismarti <artismarti@gmail.com>
2018-10-15 11:59:40 +0100

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+  if num<=1
+    return false
+  end
+  divisor = Array (2..(num - 1))
+  divisor.each do |d|
+    if num%d==0
+      return false
+    end
+  end
+  return true
+end

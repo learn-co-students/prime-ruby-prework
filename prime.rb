d0c1f4129c592ec1c5c92af461d07581a261a64e
demiansims <demiansims@gmail.com>
2018-10-17 12:17:59 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(n)
+if n <= 1
+  return false
+ end
+if n <= 3
+  return true
+ end
+
+  range = (2..n-1).to_a
+
+  range.each do |i|
+    if n % i == 0
+      return false 
+    end
+   end
+  return true
+end

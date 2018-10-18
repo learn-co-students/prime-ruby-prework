78cf0c3be83a851390d247b45b1c2cd72be9f550
rnovick13 <rnovick13@gmail.com>
2018-10-18 12:13:19 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
-# Add  code here!
+# def prime?(number)
+  # prime_test = number / number
+  # prime_test == (number / number).to_int  ? true : false
+# end
+
+def prime?(num)
+
+  if num <= 1
+     return false
+   end
+
+   if num <= 3
+     return true
+   end
+
+
+  range = (2..num-1).to_a
+
+  range.each do |i|
+    if num % i == 0
+     return false
+   end
+    end
+    return true
+  end

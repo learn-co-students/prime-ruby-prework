ce2c8630ad8577e49b3ab366b193d847a2298d49
github username <github email address>
2018-10-18 13:34:52 +0100

    Done.

--- prime.rb
+++ prime.rb
@@ -1,16 +1,11 @@
-require 'pry'
 
 def prime?(num)
-  return false  if (num.even? && num > 2) || num <= 1
-
-
-  half_num = num / 2
-  x = 3
-
-  while x < half_num
-    return false if num % x == 0
-    x += 2
+  if num <= 1
+    return false
+  elsif num <= 3
+    return true
+  else (2..num/2).none? do |x|
+    num % x == 0
   end
-    true
-
+end
 end

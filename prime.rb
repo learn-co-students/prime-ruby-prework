a67d282b7287cbe7166186fed5a6a7bc8a14cad1
Tiffany Carter <tafby88@gmail.com>
2018-10-25 04:55:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(number)
+return false if number < 2 
+newNum = number / 2 
+for n in (2..newNum) do
+  if number % n == 0 
+    return false 
+  end
+end
+return true
+end

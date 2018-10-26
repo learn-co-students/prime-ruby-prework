802e27f8ac730629681b7f76df2588fd5732ee22
Cory Adams <coadams9@gmail.com>
2018-10-26 20:48:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(num)
+  # if the number is less than or equal to one it's not prime
+  # if number is divisible by only itself and 1 a whole number of times it is prime
+  # if number is divisible by any number less than it a whole numberof times it is not prime
+  # if number is less than or equal to 1 it is not prime
+  if num <= 1
+    return false
+  end
+  # for every number starting at two and iterating up to one less than the number..
+  for x in (2...num)
+   if (num % x) == 0 #check to see if that number factors into the number evenly
+    return false #if it does that number is not a prime number
+   end
+  end
+  return true #if the number is ONLY divisible by itself and 1 a whole number of times it is prime
+end

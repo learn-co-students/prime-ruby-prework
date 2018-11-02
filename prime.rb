33b117992003a5ec79325c443e8690522984b925
sensible-proc-7557 <vchan313@gmail.com>
2018-11-02 21:07:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+def prime?(int)  
+  
+  array1 = (1..int).to_a
+  counter = 0
+   
+    array1.each do |element|
+      if int % element == 0 
+      counter += 1
+       
+      
+      end
+  end 
+ 
+
+
+ if counter == 2 and int > 1 
+    true
+  else 
+    false  
+  end
+ 
+end 
+

ab57cc307bf5353e9f0ac2b5cdff807e25c2347e
Jakob Bellamy <jakbellamy@gmail.com>
2018-11-05 03:20:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  end
+  test_set = (2...num).to_a
+  test_set.each do |test_num|
+    if num % test_num == 0
+      return false
+    end
+  end
+  return true
+end

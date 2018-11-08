97d85d3813dccdc54554a6e952dc57a2b1a2dcd1
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-08 04:48:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(n)
+  if n <= 1
+    return false
+  else
+    for i in 2..(n/2)
+      if n % i == 0
+        return false #instead tit to return falseell 
+      end
+    end
+    true
+  end
+end 

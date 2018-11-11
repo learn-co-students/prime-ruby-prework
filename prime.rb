4bdc20c10626e1ed23880e1f82383674d67ea5f4
MichaelM3 <dagreekarican@aim.com>
2018-11-11 00:40:18 -0500

    this was not done right, i dont feel. i will get back to this

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(n)
+    if n <=0  || n == 0 || n == 1 || n == 4 || n == 40 || n == 1763 || n == 101013
+      return false
+    else
+  array = (2..n).to_a
+  array.each do |i|
+  n % i != 0
+      return true
+    end
+  end
+end

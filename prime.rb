525e12f148c1f948a5da01d4fea3f66a6e84403b
Kiley Bobbitt <jkileybob@aol.com>
2018-11-13 21:03:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(integer)
+  if integer <= 1
+    return false
+  end
+  for i in 2..(integer - 1)
+  if (integer % i) == 0
+    return false
+  end
+end
+return true
+end

4bc88b3fda1b924ec3100279321b21f16a243945
MatthewBrophy <Brophy3030@gmail.com>
2018-11-14 08:24:37 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(n)
+  return false if n <= 1
+  check_range = (2..(Math.sqrt(n).to_i)).to_a
+  check_range.each {|i| return false if n % i == 0}
+  true
+end
+
+# def prime?(n)
+#   return false if n <= 1
+#   Math.sqrt(n).to_i.downto(2).each {|i| return false if n % i == 0}
+#   true
+# end

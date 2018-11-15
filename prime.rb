6c1ede39fefb40c8f30d32b6b078137c53b69744
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-15 03:19:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+ def prime?(int)
+  if int <= 1
+    return false
+  end
+  for divisor in 2..(int - 1)
+   if (int % divisor) == 0
+    return false
+   end
+  end
+  true
+ end

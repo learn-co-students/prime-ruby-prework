3a630aca50666f86c3ca8ea0b1533e8247a94357
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-16 19:08:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+def prime?(num)
+
+  if num == 2
+    return true
+  end
+  # if the number is less than 3 or divisible by zero => false
+  if num < 2 
+    return false
+  elsif num % 2 == 0
+    return false
+  end
+
+# used nested loop to see if n * m == num
+# If factor of any 2 #'s below num (non-inclusive) == num, num cannot be prime
+  (1...num).each do |n|
+    (1...num).each do |m|
+    if n * m == num
+      return false
+      end
+    end 
+  end
+  return true
+end

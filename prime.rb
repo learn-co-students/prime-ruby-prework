f770e0784ca4c5190d1419738ba0f6fedd71476a
Claire Nguyen <ms.clairenguyen@gmail.com>
2018-11-17 04:49:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(int)
+
+  if int <= 1
+    return false
+  end
+
+  range = 2...int
+  for num in range
+    if (int % num) == 0 #prime
+      return false
+    end
+  end
+  return true
+end

88ab22b7d003333b1ab20c0ec4101d77ac2cfd63
github username <github email address>
2018-11-18 14:02:46 -0500

    ShariDone

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(n)
+
+return false if n < 2
+
+for x in (2..(n - 1))
+   if (n % x) == 0
+    return false
+  #elsif n < 0
+    #  return false
+
+   end
+ end
+
+ true
+end

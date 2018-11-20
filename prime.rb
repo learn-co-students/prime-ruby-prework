ba15fbd93fc4bf60210e4af4a2dd18922c76b20a
cyn7117 <cyn7117@gmail.com>
2018-11-20 21:12:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+ def prime?(n)
+
+  if(n <= 0)
+    return false
+  elsif
+    for number in  2..n-1
+      if(n % number == 0 )
+       return false
+      end
+   else return true
+    end
+  end
+end

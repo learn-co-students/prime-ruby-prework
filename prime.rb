06646a81578d26cb18add8c5b2ffc7a044c0b032
Nison Malayev <nmalayev@gmail.com>
2018-12-03 04:05:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+
+def prime?(n)
+  if n <= 1
+    return false
+    
+  else 
+    numbers = (2..(n - 1)).to_a 
+    numbers.each do |x|
+    if (n % x) == 0
+      return false
+    #else
+      #return true
+    end
+  end
+    return true
+end
+end

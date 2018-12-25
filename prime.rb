227d3e739567fb9e65106f0f790001514c7b8209
shannon-nabors <shannon.nabors@gmail.com>
2018-12-25 18:02:34 -0500

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+def prime?(integer)
+
+  if integer < 2
+    return false
+  end
+
+  squarish_root = Math.sqrt(integer).to_i
+  relevant_range = 2..squarish_root
+
+  for number in relevant_range do
+    remainder = integer % number
+
+    if remainder == 0
+      return false
+    end
+
+  end
+
+  return true
+
+end

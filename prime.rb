ee8ca8a8ffbc983c76e4ce63ce666ff4752af1fc
github username <github email address>
2019-01-04 09:29:37 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  ## for number in 2..(number - 1)
+  ## if (number % x) == 0
+  if number >= 2
+    (2..number - 1).all? do |x|
+      number % x !=0
+    end
+  else
+    return false
+  end
+end

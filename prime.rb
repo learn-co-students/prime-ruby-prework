f8164fdf9534b0f6ef7652016ab50b56069f933c
Your Name <myung.kim287@gmail.com>
2019-01-08 20:54:25 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(int)
+  array =* (2...int)
+
+  if int <= 1
+    return false
+  end
+
+  array.each do |num|
+    if int % num == 0
+      return false
+    end
+  end
+  true
+end

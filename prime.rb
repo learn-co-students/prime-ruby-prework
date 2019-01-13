8259ce4cb7639c43685201e645182340193e1a05
Jack Rotta <jackrotta12@gmail.com>
2019-01-13 22:46:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime(number)
+  array = []
+  array.push(number)
+  for(i in array.size)
+    if(array[i] <= 1)
+      return false
+    elsif(array[i] % 2 != 0)
+      return true
+    end
+end

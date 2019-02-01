78c2d005957fb7109b4c09a967a674ae6c899403
es6ix <46784380+es6ix@users.noreply.github.com>
2019-02-01 01:11:24 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+    if number < 2
+        return false
+    end
+
+    sqroot = Math.sqrt(number)
+    for i in 2..sqroot do
+        if number % i == 0
+            return false
+        end
+    end
+
+    return true
+end

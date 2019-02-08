829f66152706a6b43fe5b9955f1bab53ae7b7b5a
Lisa Scott <lrscot@aol.com>
2019-02-08 08:15:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  end
+  (2...number).to_a.each do |divisor|
+    if number % divisor == 0
+      return false
+    end
+end
+true
+end

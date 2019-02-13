9b3c2c87d46ca6ea41cbef3475587518b47820e4
errolgh <ewatsonrei@gmail.com>
2019-02-13 15:47:38 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(integer)
+if integer < 2
+  return false
+end
+  numbers = (2...integer).to_a
+
+  numbers.each do |divisor|
+    if integer % divisor == 0
+      return false
+    end
+  end
+
+
+  true
+end
+#try to find true instances of prime numbers, before false instances

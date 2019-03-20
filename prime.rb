639d887dda5927fe35a67f2d229ce6a3a1b53684
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-20 17:50:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(number)
+  range = (2..(number - 1)).to_a
+  check = true
+  if number<2
+    check = false
+  else
+    range.each do |i|
+      if number % i == 0
+        check = false
+        break
+      end
+    end
+  end
+ check
+        
+end

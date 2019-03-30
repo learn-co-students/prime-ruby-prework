6846fed7fc819c2b3276afee654a4ca7b3fc2c32
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-03-30 19:23:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 
-def prime?(num) 
+def prime2?(num) # This without iteration just for practice.  I'm trying to code less #convoluted code so I am practicing.  So far I am failing.
   primeNumArr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
    #check aginst arr 
    
@@ -28,4 +28,22 @@ def prime?(num)
     end   
   end 
   return returner
-end    
+end  
+def prime?(num)
+  primeNumArr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
+  num2 = 0 
+  returner = ""
+  primeNumArr.each do 
+    if num == primeNumArr[num2]
+      return true
+    end 
+    if num <= 1 
+      return false 
+    end   
+    if num % primeNumArr[num2] == 0
+      return false 
+    end 
+    num2 += 1
+  end
+  return true
+end  

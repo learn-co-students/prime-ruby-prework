0af73e62839a5a9bd6aa7f52163aeb3ba711b237
Amy Cen <amy.cen@blackrock.com>
2019-04-14 05:20:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+def prime?(n)
+  ans = true
+  if n <= 1
+    ans = false
+  elsif n == 2 || n == 3
+    ans = true
+  elsif n % 2 == 0 || n % 3 == 0
+    ans = false
+  end
+
+  i = 5
+  w = 2
+
+  while i * i <= n
+    if n % i == 0
+      ans = false
+    end
+    i += w
+    w = 6 - w
+  end
+  ans
+end

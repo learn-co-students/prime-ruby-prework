eb7034ab9b64b62ad453cf09a38908802acaa70f
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-05-23 11:15:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(integer)
+  if integer < 2
+    return false
+  end
+  range = 2...integer
+  range.to_a.each do | number |
+    if integer % number == 0
+      return false
+    end
+  end
+  true
+end

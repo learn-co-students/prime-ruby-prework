9cbc51b8850ef8fbc045c7fe776caab83ce20dfa
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-30 02:26:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,9 +1,17 @@
+=begin
 def prime?(n)
   return false if n < 2
   return true if n == 2
-    if (2...n).any?{|i| n % i == 0}
-      false
-    else
-      true
-    end
+  if (2...n).any?{|i| n % i == 0}
+    false
+  else
+    true
+  end
+end
+=end
+
+def prime?(n)
+  return false if n < 2
+  return true if n == 2
+  !(2...n).any?{|i| n % i == 0}
 end

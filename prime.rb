4b3fd853e318d759132e191ce0aa3ad4c1ccbfd1
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-06-04 20:42:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -3,9 +3,11 @@ def prime? (num)
   if num <= 1 
     return false 
   elsif (2..(num-1)).each do |i|
-    if (n % i) == 0 
+    if (num % i) == 0 
       return false
     end
+  end
   return TRUE
+end
 end
 

40d06ab933b2b82de871f740028681da783f9937
github username <github email address>
2019-06-23 22:25:09 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+
+def prime?(num)
+    if num <= 1
+        return false
+    end
+    (2..Math.sqrt(num)).each do |i| 
+        if num % i == 0
+            return false
+        end
+    end
+    true
+end

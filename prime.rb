a5a329b859ac45ec8e6f65f7c3c1057139cf20b2
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-23 22:25:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+ 
+ def prime?(n)
+   num = n.abs
+  return false if num < 2
+   is_prime = true
+   range = 2..(Math.sqrt(num))
+   for number in range do
+     if num%number == 0 
+       is_prime = false
+     end
+   end
+    return is_prime
+  end
+       
+   
